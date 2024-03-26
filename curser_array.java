eefb4306984cde2e403b58b3ffdec77b4c868c59
mohammad n nimer <139014861+mohammednnimer@users.noreply.github.com>
2024-03-27 00:28:05 +0200

    curser_array.java

--- curser_array.java
+++ curser_array.java
@@ -36,7 +36,7 @@ public class curser_array<T extends Comparable<T> > {
 			return;
 		}
 
-		newnode[p].next = newnode[0].next;
+		newnode[p] =new  CNode<>(null, newnode[0].next);
 		newnode[0].next = p;
 
 	}
@@ -51,26 +51,66 @@ return n;
 System.out.println("sorry the list is full");
 		return 0;
 	}
+	public boolean isnull(int l)
+	{
+		
+		return newnode[l].data==null;
+		
+	}
+	
+	
 
 
 public void insertFirst(T data,int l)
 {
-
+ if(isnull(l))
+	 return;
+	
+	
 	int p=malloc();
 	if(p!=0)
 	{
-		newnode[p].data=data;
-		newnode[p].next=newnode[l].next;
+		newnode[p]=new CNode<>(data,newnode[l].next);
+		
 		newnode[l].next=p;
 		
+
 		
-		
-		
+	}
+	else 
+	{
+		System.out.println("full");
 	}
 	
 	
 	
 }
+public void tracverce(int n)
+{if(isnull(n))
+{
+	return;
+	
+}
+	
+	
+	
+	int l=newnode[n].next;
+	while (l!=0) {
+
+		System.out.print(newnode[l].data+"-->");
+		l=newnode[l].next;
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
 public void insertlast(T data,int l)
 {
 	int p=malloc();
@@ -120,15 +160,16 @@ int p=malloc();
 if(p!=0) {
 if(newnode[l]!=null) {	
 
-	
 newnode[p]=new CNode<>(data,0);
 int first=newnode[l].next;
 
 
 if(newnode[first].data==null)
 {
+	
+	
 
-	newnode[p].next = first;
+	newnode[p].next = 0;
 	
     newnode[l].next = p;
 
@@ -137,22 +178,33 @@ if(newnode[first].data==null)
 }
 
 int last=first;
-while (newnode[first].data.compareTo(newnode[p].data)<0&&newnode[newnode[first].next].data!=null) {
-	System.out.println(newnode[first].data);
+if(newnode[first].data.compareTo(newnode[p].data)>0)
+{
+	
+	
+	newnode[p].next=newnode[l].next;
+	newnode[l].next=p;
+	
+	return;
+	
+}
+while (newnode[first].data.compareTo(newnode[p].data)<0&&newnode[first].next!=0) {
+
 	last=first;
-	System.out.println("llllleeeshhh");
+	System.out.println(last);
 	first=newnode[first].next;
 
+
 }
-if(newnode[newnode[first].next].data!=null&&newnode[first].data.compareTo(newnode[p].data)<0)
+if(newnode[first].next==0&&newnode[first].data.compareTo(newnode[p].data)<0)
 {
 	
 	newnode[p].next=0;
-	newnode[last].next=p;
+	newnode[first].next=p;
 	
 
 }
-else if(newnode[newnode[first].next].data==null)
+else if(newnode[first].next==0)
 {
 	newnode[p].next=first;
 	newnode[last].next=p;
@@ -164,6 +216,7 @@ else if(newnode[newnode[first].next].data==null)
 
 	newnode[p].next=first;
 	newnode[last].next=p;
+
 	
 }
 
